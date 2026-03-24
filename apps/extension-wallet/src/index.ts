9948e07efce56ee23786d7bd9b8742e80e4a7894
David-patrick-chuks <pd3072894@gmail.com>
2026-03-24 07:00:58 +0100

    feat(extension-wallet): add popup router and navigation

--- apps/extension-wallet/src/index.ts
+++ apps/extension-wallet/src/index.ts
@@ -1,3 +1,6 @@
 export * from './components/TransactionStatus';
+export * from './components/Navigation/NavBar';
+export * from './router';
+export * from './router/AuthGuard';
 export * from './screens/TransactionDetail';
 export * from './utils/explorer-links';
--- apps/extension-wallet/src/main.tsx
+++ apps/extension-wallet/src/main.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { SettingsScreen } from './screens/Settings/SettingsScreen';
+import { ExtensionRouter } from './router';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <div className="w-[360px] min-h-screen bg-background mx-auto shadow-xl">
-      <SettingsScreen />
-    </div>
+    <ExtensionRouter />
   </React.StrictMode>
 );
