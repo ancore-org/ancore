49fc3292187dde12f97eda0d912462c5f8ac3e2a
Okoli Johnpaul Sochimaobi <132228270+Johnpii1@users.noreply.github.com>
2026-05-28 16:09:57 +0100

    feat(mobile-wallet): bootstrap MVP shell

--- apps/mobile-wallet/src/index.ts
+++ apps/mobile-wallet/src/index.ts
@@ -1,2 +1,7 @@
+export * from './accounts';
+export * from './app';
+export * from './config/environment';
+export * from './navigation';
+export * from './sdk';
 export * from './security';
 export * from './storage';
